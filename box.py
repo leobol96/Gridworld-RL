0dde53d1b5f95ed987bfd415c563e88e5612bf77
Leonardo <leobol96@gmail.com>
2020-12-22 11:22:35 +0100

    First commit, still bug with walls

---
+++ box.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+
+class Box(object):
+
+    class Direction(Enum):
+        UP = 'up'
+        DOWN = 'down'
+        LEFT = 'left'
+        RIGHT = 'right'
+
+    def __init__(self, reward: float, is_wall=False, q_a=None, v_pi=0, terminal=False):
+        if q_a is None:
+            q_a = {self.Direction.UP.value: 0, self.Direction.DOWN.value: 0, self.Direction.LEFT.value: 0, self.Direction.RIGHT.value: 0}
+
+        self.reward = reward
+        self.terminal = terminal
+        self.q_a = q_a
+        self.v_pi = v_pi
+        self.wall = is_wall
+
+    def get_max_q(self, value_type):
+        max_value = None
+        for possible_direction in [*self.q_a]:
+            if max_value is None:
+                max_value = self.q_a[possible_direction]
+                max_q = possible_direction
+            else:
+                if self.q_a[possible_direction] > max_value:
+                    max_value = self.q_a[possible_direction]
+                    max_q = possible_direction
+        if value_type == 'action':
+            return max_q
+        else:
+            return max_value
---
+++ grid_world.py
@@ -0,0 +1,185 @@
+import random
+
+from box import Box
+import matplotlib.pyplot as plt
+
+
+class GridWord(object):
+    """
+    This class represent the gridworld.
+    """
+
+    def __init__(self, height, width, r_nt=0):
+
+        self.episode = 1
+        self.rewards_during_episode = []
+        self.current_state = [0, 0]
+        self.world = []
+        for col in range(width):
+            tmp = []
+            for row in range(height):
+                # Define a Box and for all the box remove the directions that bring the agents outside of the world
+                box = Box(r_nt)
+                if row == 0:
+                    box.q_a.pop(Box.Direction.LEFT.value)
+                if row == width - 1:
+                    box.q_a.pop(Box.Direction.RIGHT.value)
+                if col == 0:
+                    box.q_a.pop(Box.Direction.UP.value)
+                if col == height - 1:
+                    box.q_a.pop(Box.Direction.DOWN.value)
+                tmp.append(box)
+            self.world.append(tmp)
+
+    def set_terminal_state(self, row: int, col: int, reward: float) -> None:
+        """
+        This method is used to set terminal states inside the GridWorld.
+        Args:
+            row: Row of the terminal state
+            col: Column of the terminal state
+            reward: Reward getting arriving in that terminal state
+        """
+        self.world[row][col].reward = reward
+        self.world[row][col].terminal = True
+        self.world[row][col].wall = False
+
+    def set_wall(self, walls: list) -> None:
+        """
+        Method used to set the walls inside the gridworld.
+        Args:
+            walls: List containing positions (x,y)
+        """
+        for wall in walls:
+            self.world[wall[0]][wall[1]].wall = True
+
+    def plot_world(self, variable: str = 'v_pi'):
+        """
+        Method used to to plot the world
+        Args:
+            variable: Type of variable visualized in the plot
+        """
+        fig, ax = plt.subplots()
+        matrix_tmp = []
+        matrix_tmp_a = []
+        for col in self.world:
+            tmp = []
+            tmp_a = []
+            for row in col:
+                if variable == 'v_pi':
+                    tmp.append(row.v_pi)
+                elif variable == 'wall':
+                    tmp.append(int(row.wall))
+                elif variable == 'reward':
+                    tmp.append(row.reward)
+                elif variable == 'q_a':
+                    tmp_a.append(row.get_max_q('action'))
+                    tmp.append(round(row.get_max_q('value'),1))
+                elif variable == 'terminal':
+                    tmp.append(int(row.terminal))
+            matrix_tmp.append(tmp)
+            matrix_tmp_a.append(tmp_a)
+
+        ax.matshow(matrix_tmp, cmap=plt.cm.Greens)
+        plt.title(variable)
+
+        if variable in ['v_pi', 'reward', 'q_a']:
+            for idx_col, col in enumerate(matrix_tmp):
+                for idx_row, row in enumerate(col):
+                    if variable == 'q_a':
+                        ax.text(idx_row, idx_col, str(row), va='bottom', ha='center')
+                        ax.text(idx_row, idx_col, matrix_tmp_a[idx_col][idx_row], va='top', ha='center')
+                    else:
+                        ax.text(idx_col, idx_row, str(row), va='center', ha='center')
+        plt.show()
+
+    def action_e_greedy(self, epsilon):
+        epsilon = epsilon * 100
+        q_current_state = self.world[self.current_state[0]][self.current_state[1]].q_a
+        possible_direction = [*q_current_state]
+        value = random.choices(['random', 'greedy'], weights=[epsilon, 100 - epsilon], k=1)
+
+        # Remove all the directions were a wall is found
+        for direction in possible_direction[:]:
+            if self.direction_against_wall(direction):
+                possible_direction.remove(direction)
+
+        # Choose greedy between the possible directions
+        if 'greedy' in value:
+            # take maximum
+            action = self.world[self.current_state[0]][self.current_state[1]].get_max_q(value_type='action')
+        else:
+            action = random.choice(possible_direction)
+        return action
+
+    def get_next_state(self, direction):
+        col = self.current_state[0]
+        row = self.current_state[1]
+
+        if direction == Box.Direction.DOWN.value:
+            col = col + 1
+        elif direction == Box.Direction.UP.value:
+            col = col - 1
+        elif direction == Box.Direction.RIGHT.value:
+            row = row + 1
+        elif direction == Box.Direction.LEFT.value:
+            row = row - 1
+        return [col, row]
+
+    def direction_against_wall(self, direction):
+        col, row = self.get_next_state(direction=direction)
+        if self.world[col][row].wall:
+            return True
+
+    def update_sarsa(self, direction):
+        pass
+
+    def update_q_learning(self, direction, alpha, discount_factor):
+        # Update Q
+        col, row = self.get_next_state(direction=direction)
+        self.world[self.current_state[0]][self.current_state[1]].q_a[direction] = \
+        self.world[self.current_state[0]][self.current_state[1]].q_a[direction] + alpha * (self.world[col][row].reward + (
+                    discount_factor * self.world[col][row].get_max_q(value_type='value')) -
+                                                                                           self.world[
+                                                                                               self.current_state[0]][
+                                                                                               self.current_state[
+                                                                                                   1]].q_a[direction])
+        self.rewards_during_episode.append(self.world[col][row].reward)
+        # Update current state
+        if self.world[col][row].terminal:
+            self.current_state[0] = 0
+            self.current_state[1] = 0
+            print(self.episode)
+            print(sum(self.rewards_during_episode))
+            self.episode = self.episode + 1
+        else:
+            self.current_state[0] = col
+            self.current_state[1] = row
+
+    def update_state(self):
+        pass
+
+
+if __name__ == '__main__':
+    gridworld = GridWord(height=9, width=9, r_nt=-1)
+    gridworld.set_terminal_state(row=8, col=8, reward=50)
+    gridworld.set_terminal_state(row=6, col=6, reward=-50)
+    gridworld.set_wall([[1, 2],
+                        [1, 3],
+                        [1, 4],
+                        [1, 5],
+                        [1, 6],
+                        [2, 6],
+                        [3, 6],
+                        [4, 6],
+                        [5, 6],
+                        [2, 6],
+                        [7, 1],
+                        [7, 2],
+                        [7, 3],
+                        [7, 4]])
+    # gridworld.plot_world('wall')
+    # gridworld.plot_world('terminal')
+    while gridworld.episode <= 100:
+        action = gridworld.action_e_greedy(0.90)
+        gridworld.update_q_learning(direction=action, alpha=0.99, discount_factor=1)
+    gridworld.plot_world('q_a')
